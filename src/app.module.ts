e15958afa7b79381c37882aa37ffdb530f963198
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2025-11-04 13:30:44 -0300

    conexion con BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromocionModule } from './promocion/promocion.module';
 import { DiaModule } from './dia/dia.module';
+import { Promocion } from './entities/promocion.entity';
+import { Dia } from './entities/dia.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PromocionModule, DiaModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'promocion_db',
+      username: 'postgres',
+      password: 'mipassword',
+      synchronize: true,
+      entities: [Promocion, Dia],
+    }),
+    PromocionModule,
+    DiaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
