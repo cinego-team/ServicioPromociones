a210265aa39a627f14c06482f36479e8a0d4bf1d
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-04 19:25:15 -0300

    aplicacion de variables de entorno

--- src/axios_service/env.ts
+++ src/axios_service/env.ts
@@ -1,8 +1,8 @@
 export const config = {
-  APIUsuariosUrls: {
-    baseUrl: 'http://localhost:3000',
-    validarTipoCliente: (id) => `/microservicio-usuarios/tipo-cliente/validar-existencia-tipoCliente/${id}`,
-    getTipoClienteById: (id: number) => `/microservicio-usuarios/tipo-cliente/admin/${id}`,
+    APIUsuariosUrls: {
+        baseUrl: process.env.URL_MS_USUARIOS || 'http://localhost:3000',
+        validarTipoCliente: (id) => `/microservicio-usuarios/tipo-cliente/validar-existencia-tipoCliente/${id}`,
+        getTipoClienteById: (id: number) => `/microservicio-usuarios/tipo-cliente/admin/${id}`,
 
-  },
+    },
 };
