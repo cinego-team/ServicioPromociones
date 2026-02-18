dc6910f545c13d48cf98f539ae73aed6070cb960
Valentino <valentino.pajon@gmail.com>
2026-02-18 20:18:11 -0300

    mini fix puerto

--- src/axios_service/env.ts
+++ src/axios_service/env.ts
@@ -1,6 +1,6 @@
 export const config = {
     APIUsuariosUrls: {
-        baseUrl: `http://localhost:${process.env.PUERTO_MS_USUARIOS}`,
+        baseUrl: `http://localhost:3004`,
         validarTipoCliente: (id) => `/microservicio-usuarios/tipo-cliente/validar-existencia-tipoCliente/${id}`,
         getTipoClienteById: (id: number) => `/microservicio-usuarios/tipo-cliente/admin/${id}`,
 
