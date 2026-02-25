49ad332a24ba51e6008ddd579a7f597e90d3d6dd
Valentino <valentino.pajon@gmail.com>
2026-02-25 19:43:46 -0300

    Fix front empleado 25/02

--- src/axios_service/env.ts
+++ src/axios_service/env.ts
@@ -1,11 +1,8 @@
-import { get } from "axios";
-
 export const config = {
     APIUsuariosUrls: {
         baseUrl: `http://localhost:3004`,
         getUserById: (id: number) => `/usuario/api/${id}`,
-        validarTipoCliente: (id) => `/microservicio-usuarios/tipo-cliente/validar-existencia-tipoCliente/${id}`,
-        getTipoClienteById: (id: number) => `/microservicio-usuarios/tipo-cliente/admin/${id}`,
-
+        validarTipoCliente: (id) => `/tipo-cliente/validar-existencia/${id}`,
+        getTipoClienteById: (id: number) => `/tipo-cliente/admin/${id}`,
     },
 };
